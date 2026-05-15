5d7f4d4c2910e31a5d07018fe37bb2d9be510e00
seoyun-lee <20212025@o365.hanbat.ac.kr>
2026-05-15 14:09:22 +0900

    refactor: 개인 정보 관리 로직을 전용 mypage 모듈로 분리

---
+++ src/modules/mypage/mypage.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module.js';
+import { MypageController } from './mypage.controller.js';
+import { MypageService } from './mypage.service.js';
+
+@Module({
+    imports: [AuthModule],
+    controllers: [MypageController],
+    providers: [MypageService],
+    exports: [MypageService],
+})
+export class RoomModule {}
